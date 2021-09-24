727cb199c0d649a3844eadc5470f51f92918e296
aloisiot <loistc.carvalho@gmail.com>
2021-09-24 17:38:29 -0300

    Finalizando autenticaçao

--- js/login.js
+++ js/login.js
@@ -1,14 +1,31 @@
+window.onload = render;
+
+
 function autentication() {
-    const user = {
-        userName: document.getElementById('User'),
-        pass: document.getElementById('Pass')
-    }
+
+        const userName= document.getElementById('User').value;
+        const pass = document.getElementById('Pass').value;
+
     let users = window.localStorage.getItem("users");
     users = JSON.parse(users);
-
-    let isValid = users.find(e => {
-        return e = user
+    
+    let isValid = false;
+    isValid = users.find(e => {
+        return e.userName === userName && e.pass === pass;
     })
 
-        console.log(isValid);
+    console.log(isValid)
+    
+    if(isValid == false || isValid == undefined || isValid == null || isValid == ''){
+        alert("Usuario inválido!");
+    }else{
+        window.location.pathname = "/html/todo.html"
+    }
+}
+
+const submitButton = document.getElementById("submit-button");
+submitButton.onclick = ev => {
+    ev.preventDefault();
+
+    autentication();
 }
